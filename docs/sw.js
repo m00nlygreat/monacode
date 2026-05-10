1cc3cdc0d300f92c4f12c5dec8b7b3065840e223
m00nlygreat <moonlygreat@gmail.com>
2026-05-11 04:55:25 +0900

    Update PWA service worker cache strategy

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'monacode-v1';
+const CACHE_NAME = 'monacode-v2';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
@@ -6,7 +6,19 @@ self.addEventListener('install', (event) => {
   );
 });
 
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) => Promise.all(names.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))))
+  );
+});
+
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(fetch(event.request).catch(() => caches.match('./index.html')));
+    return;
+  }
+
   event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
 });
